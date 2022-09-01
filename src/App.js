ae8be0e99eac575ea20acfc59a3f46a7befb1a34
saif8983 <asaif8367@gmail.com>
2022-09-02 01:30:39 +0530

    initial face

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <><div class="container">
+      <div class="navbarBar"></div>
+      <div class="mainContainer"></div>
+    </div></>
+  );
+}
+
+export default App;
